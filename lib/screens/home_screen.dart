84a4e7b8dc7c259e0963405442cffaa0348edd08
Jonathan <willyjo1@ADMINs-MacBook-Pro.local>
2023-05-11 23:32:17 +0000

    category screen created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/info/data.dart';
+import 'package:news_app/model/category_model.dart';
+import 'package:news_app/widgets/category_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,30 +11,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<CategoryModel> category = List<CategoryModel>.empty(growable: true);
+  @override
+  void initState() {
+    super.initState();
+    category = getCategoryModel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: RichText(text: const TextSpan(
-          children: [
+        title: RichText(
+            text: const TextSpan(children: [
           TextSpan(
-            text: "Flutter",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20
-            )
-          ),
+              text: "Flutter",
+              style: TextStyle(color: Colors.black, fontSize: 20)),
           TextSpan(
-            text: "News",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue
-            )
+              text: "News", style: TextStyle(fontSize: 20, color: Colors.blue))
+        ])),
+      ),
+      body: Container(
+        
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            height: 70,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+                itemCount: category.length,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                      imageUrl: category[index].imageUrl,
+                      categoryName: category[index].categoryName);
+                }),
           )
-        ] )
-        ),
+        ]),
       ),
     );
   }
--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -8,8 +8,25 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(right: 16),
       child: Stack(children: [
-        Image.network(imageUrl)
+        ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: Image.network(imageUrl, width: 140, height: 80,fit: BoxFit.cover,)),
+        Container(
+          width: 140, 
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Center(
+            child: Text(categoryName, 
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600),)),
+        )
       ]),
     );
   }
